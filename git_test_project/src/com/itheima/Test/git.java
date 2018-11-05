55b757bf36eaad9f131c699556a7243d1d71c9da
yangwen <18507096009@163.com>
2018-11-05 19:47:58 +0800

    本地工程添加到get

---
+++ git_test_project/src/com/itheima/Test/git.java
@@ -0,0 +1,11 @@
+package com.itheima.Test;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+        System.out.println("hellow world");
+        System.out.println("hellow world");
+        System.out.println("hellow world");
+        System.out.println("hellow world");
+    }
+}
