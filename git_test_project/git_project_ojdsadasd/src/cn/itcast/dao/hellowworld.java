65a51155d3fbc4cb3a6b36fece11d0bafa5f09e5
yangwen <18507096009@163.com>
2018-11-05 21:20:28 +0800

    本地工程添加到get

--- git_test_project/git_project_ojdsadasd/src/cn/itcast/dao/hellowworld.java
+++ git_test_project/git_project_ojdsadasd/src/cn/itcast/dao/hellowworld.java
@@ -3,5 +3,16 @@ package cn.itcast.dao;
 public class hellowworld {
     public static void main(String[] args) {
         System.out.println("make love  love");
+
+        int a=1;
+
+        int b=2;
+
+        System.out.println(a+b);
     }
+
+
+
+
+
 }
